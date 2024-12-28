f9395e327f7fcd7b83013c09cbeb5fad7a56fcee
NelsonCoder525 <nelsonr525@hotmail.com>
2024-12-28 18:03:22 -0300

    final do 15

--- barbearia/settings/prod.py
+++ barbearia/settings/prod.py
@@ -3,6 +3,7 @@ import os
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
-SECRET_KEY = ''
+SECRET_KEY = os.environ.get("SECRET_KEY")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'barbearia.settings.prod'))
 
 print(f"DJANGO_SETTINGS_MODULE: {os.getenv('DJANGO_SETTINGS_MODULE')}")
