2deb979abfc442f620962262d7cc903d7e51490a
NelsonCoder525 <nelsonr525@hotmail.com>
2025-01-04 16:49:54 -0300

    createsuperuser on render

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from agenda.models import Agendamento
+from agenda.models import Agendamento, Cliente, Fidelidade, Endereco
 
 # Register your models here.
 admin.site.register(Agendamento)
+admin.site.register(Cliente)
+admin.site.register(Fidelidade)
+admin.site.register(Endereco)
--- barbearia/settings/base.py
+++ barbearia/settings/base.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'barbearia.urls'
@@ -72,6 +73,10 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 WSGI_APPLICATION = 'barbearia.wsgi.application'
 
