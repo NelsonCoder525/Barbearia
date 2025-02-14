69f54829300f36ceed9de7e60b9bd05046fb161d
NelsonCoder525 <nelsonr525@hotmail.com>
2025-02-14 17:16:45 -0300

    render

--- barbearia/settings/prod.py
+++ barbearia/settings/prod.py
@@ -6,4 +6,17 @@ ALLOWED_HOSTS = ['*']
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'barbearia.settings.prod'))
 
+
+
 print(f"DJANGO_SETTINGS_MODULE: {os.getenv('DJANGO_SETTINGS_MODULE')}")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresqlpsycopg2',
+        'NAME': 'barbearia_db',
+        'USER': 'admin',
+        'PASSWORD': 'FhEWTcuajahidrEIYiaLlfYVpZabxei1',
+        'HOST': 'dpg-cunp855umphs73bp9ar0-a.oregon-postgres.render.com',
+        'PORT': '5432',
+    }
+}
