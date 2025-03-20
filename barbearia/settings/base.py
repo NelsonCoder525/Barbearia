062098ff6e6d1861dd922b100bfe6096f2fdcbdf
NelsonCoder525 <nelsonr525@hotmail.com>
2025-03-20 19:02:18 -0300

    celery

--- barbearia/settings/base.py
+++ barbearia/settings/base.py
@@ -171,5 +171,5 @@ EMAIL_PORT = os.environ.get('EMAIL_PORT','1025')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER','')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD','')
 
-CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/0')
-CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/0')
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://172.22.11.228:6379/0')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://172.22.11.228:6379/0')
